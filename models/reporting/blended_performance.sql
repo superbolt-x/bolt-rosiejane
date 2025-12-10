e4ccbccbca2bf377b9e0f591b65c2b04989df611
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-12-10 15:23:55 -0300

    Refine TikTok campaign filters and update channel names
    Added campaign id filter for meta sephora and segmented tiktok

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -12,7 +12,7 @@ WITH paid_data as
         (SELECT 'Meta Sephora' as channel, date, date_granularity, 
             spend, link_clicks as clicks, impressions, purchases, revenue
         FROM {{ source('reporting','facebook_campaign_performance') }}
-        WHERE campaign_name ~* 'sephora'
+        WHERE campaign_name ~* 'sephora' AND campaign_id = 23852671081560313
         UNION ALL
         SELECT 'Meta DTC' as channel, date, date_granularity, 
             spend, link_clicks as clicks, impressions, purchases, revenue
@@ -23,37 +23,42 @@ WITH paid_data as
             spend, clicks, impressions, purchases, revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
         UNION ALL
-        SELECT 'TikTok DTC' as channel, date, date_granularity, 
+        SELECT 'TikTok DTC Prospecting' as channel, date, date_granularity, 
             spend, clicks, impressions, purchases, revenue
         FROM {{ source('reporting','tiktok_ad_performance') }}
-        WHERE campaign_name !~* 'traffic'
+        WHERE campaign_name !~* 'traffic' and campaign_name ~* 'prospecting'
+        UNION ALL
+        SELECT 'TikTok DTC Retargeting' as channel, date, date_granularity, 
+            spend, clicks, impressions, purchases, revenue
+        FROM {{ source('reporting','tiktok_ad_performance') }}
+        WHERE campaign_name !~* 'traffic' and campaign_name ~* 'retargeting'
         UNION ALL
         SELECT 'TikTok Sephora' as channel, date, date_granularity, 
             spend, clicks, impressions, purchases, revenue
         FROM {{ source('reporting','tiktok_ad_performance') }}
         WHERE campaign_name ~* 'traffic'
         UNION ALL
-        SELECT 'Tiktok DTC' as channel, date_trunc('day',date) as date, 'day' as date_granularity, 
+        SELECT 'Tiktok GMV' as channel, date_trunc('day',date) as date, 'day' as date_granularity, 
             sum(spend) as spend, sum(0) as clicks, sum(0) as impressions, sum(purchases) as purchases, sum(revenue) as revenue
         FROM {{ source('gsheet_raw','tiktok_gmv_campaigns_insights') }}
         GROUP BY 1,2,3
         UNION ALL
-        SELECT 'Tiktok DTC' as channel, date_trunc('week',date) as date, 'week' as date_granularity, 
+        SELECT 'Tiktok GMV' as channel, date_trunc('week',date) as date, 'week' as date_granularity, 
             sum(spend) as spend, sum(0) as clicks, sum(0) as impressions, sum(purchases) as purchases, sum(revenue) as revenue
         FROM {{ source('gsheet_raw','tiktok_gmv_campaigns_insights') }}
         GROUP BY 1,2,3
         UNION ALL
-        SELECT 'Tiktok DTC' as channel, date_trunc('month',date) as date, 'month' as date_granularity, 
+        SELECT 'Tiktok GMV' as channel, date_trunc('month',date) as date, 'month' as date_granularity, 
             sum(spend) as spend, sum(0) as clicks, sum(0) as impressions, sum(purchases) as purchases, sum(revenue) as revenue
         FROM {{ source('gsheet_raw','tiktok_gmv_campaigns_insights') }}
         GROUP BY 1,2,3
         UNION ALL
-        SELECT 'Tiktok DTC' as channel, date_trunc('quarter',date) as date, 'quarter' as date_granularity, 
+        SELECT 'Tiktok GMV' as channel, date_trunc('quarter',date) as date, 'quarter' as date_granularity, 
             sum(spend) as spend, sum(0) as clicks, sum(0) as impressions, sum(purchases) as purchases, sum(revenue) as revenue
         FROM {{ source('gsheet_raw','tiktok_gmv_campaigns_insights') }}
         GROUP BY 1,2,3
         UNION ALL
-        SELECT 'Tiktok DTC' as channel, date_trunc('year',date) as date, 'year' as date_granularity, 
+        SELECT 'Tiktok GMV' as channel, date_trunc('year',date) as date, 'year' as date_granularity, 
             sum(spend) as spend, sum(0) as clicks, sum(0) as impressions, sum(purchases) as purchases, sum(revenue) as revenue
         FROM {{ source('gsheet_raw','tiktok_gmv_campaigns_insights') }}
         GROUP BY 1,2,3
